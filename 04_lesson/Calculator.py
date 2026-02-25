6e4856da2719b392cff66fca810e0745e7603885
Silence <zamani1908@mail.ru>
2026-02-26 00:11:22 +0700

    lesson4

---
+++ 04_lesson/Calculator.py
@@ -0,0 +1,27 @@
+class Calculator:
+
+    def sum(a, b):
+        result = a+b
+        return result
+    
+    def sub(a, b):
+        result = a-b
+        return reslut
+    
+    def mul(a, b):
+        return a*b
+    
+    def div(a, b):
+        if b == 0:
+            raise ArithmeticError('На ноль делить нельзя')
+        
+        return a/b
+    
+    def pow(a, b=2):
+        return a**b
+    
+    def avg(nums):
+        
+#     pow(2,3)
+
+# print Calculator
